10ab78c5336226be79edc3c68ac3c4e58d1189cc
Vera Peeters <vera.peeters@thomasmore.be>
2024-11-14 21:11:44 +0100

    define relations via name/title, not via id

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,83 @@
-insert into BOOK (TITLE) values ('Oryx and Crake'); /*1*/
-insert into BOOK (TITLE) values ('The year of the flood');/*2*/
-insert into BOOK (TITLE) values ('MaddAddam');/*3*/
-insert into BOOK (TITLE) values ('1Q84');/*4*/
-insert into BOOK (TITLE) values ('De opwindvogelkronieken');/*5*/
-insert into BOOK (TITLE) values ('Design Patterns');/*6*/
+insert into BOOK (TITLE)
+values ('Oryx and Crake'); /*1*/
 
-insert into AUTHOR (NAME) values ('Margaret Atwood'); /*1*/
-insert into AUTHOR (NAME) values ('Haruki Murakami'); /*2*/
-insert into AUTHOR (NAME) values ('Erich Gamma'); /*3*/
-insert into AUTHOR (NAME) values ('Richard Helm'); /*4*/
-insert into AUTHOR (NAME) values ('Ralph Johnson'); /*5*/
-insert into AUTHOR (NAME) values ('John Vlissides'); /*6*/
+insert into BOOK (TITLE)
+values ('The year of the flood');/*2*/
 
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (1, 1);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (2, 1);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (3, 1);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (4, 2);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (5, 2);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (6, 3);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (6, 4);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (6, 5);
-insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID) values (6, 6);
+insert into BOOK (TITLE)
+values ('MaddAddam');/*3*/
 
-insert into GENRE(NAME)values ('fantasy');
-insert into GENRE(NAME)values ('non-fiction');
-insert into GENRE(NAME)values ('programming');
+insert into BOOK (TITLE)
+values ('1Q84');/*4*/
+
+insert into BOOK (TITLE)
+values ('De opwindvogelkronieken');/*5*/
+
+insert into BOOK (TITLE)
+values ('Design Patterns');/*6*/
+
+insert into AUTHOR (NAME)
+values ('Margaret Atwood'); /*1*/
+
+insert into AUTHOR (NAME)
+values ('Haruki Murakami'); /*2*/
+
+insert into AUTHOR (NAME)
+values ('Erich Gamma'); /*3*/
+
+insert into AUTHOR (NAME)
+values ('Richard Helm'); /*4*/
+
+insert into AUTHOR (NAME)
+values ('Ralph Johnson'); /*5*/
+
+insert into AUTHOR (NAME)
+values ('John Vlissides'); /*6*/
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'Oryx and Crake',
+           select id from AUTHOR where name = 'Margaret Atwood');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'The year of the flood',
+           select id from AUTHOR where name = 'Margaret Atwood');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'MaddAddam',
+           select id from AUTHOR where name = 'Margaret Atwood');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= '1Q84',
+           select id from AUTHOR where name = 'Haruki Murakami');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'De opwindvogelkronieken',
+           select id from AUTHOR where name = 'Haruki Murakami');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'Design Patterns',
+           select id from AUTHOR where name = 'Erich Gamma');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'Design Patterns',
+           select id from AUTHOR where name = 'Richard Helm');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'Design Patterns',
+           select id from AUTHOR where name = 'Ralph Johnson');
+
+insert into BOOK_AUTHORS (BOOKS_ID, AUTHORS_ID)
+values (select id from BOOK where title= 'Design Patterns',
+           select id from AUTHOR where name = 'John Vlissides');
+
+insert into GENRE(NAME)
+values ('fantasy');
+
+insert into GENRE(NAME)
+values ('non-fiction');
+
+insert into GENRE(NAME)
+values ('programming');
 
 INSERT INTO BOOKSUSER (USERNAME, PASSWORD, ROLE)
 VALUES ('admin', '$2a$10$9MIX8kYPkuB7uE/H5nHF8.KG6.YdjBA/voOnjSZnZDxLXL/2BIerS', 'ADMIN'); -- admin
